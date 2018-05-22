b11660ee3ba92b58e99466f13726a8a8e7a3e108
quimicobr <davi.nl@hotmail.com>
2018-05-22 09:36:05 -0300

    Update serial.h

--- serial.h
+++ serial.h
@@ -8,6 +8,7 @@ extern idata char pacote[30];
 void configura_serial();
 void escreve_serial(char* mensagem);
 void trata_interrupcao_serial();
+void le_parametros();
 char trata_dados();
 void solicita_senador(char* codigo);
 void solicita_governador(char* codigo);
